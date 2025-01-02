7c7f2494dd8a80b0da88425849e50a3ab052ba13
bforcum <bmforcum@outlook.com>
2025-01-01 23:05:14 -0800

    Render Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "config.h"
+#include "triangle_mesh.h"
 
 using namespace std;
 
@@ -12,21 +13,19 @@ unsigned int make_module(const std::string& filepath, unsigned int module_type);
 
 int main() {
 
-	std::ifstream file;
-	std::string line;
-	file.open("shader/vertex.txt");
-	while (std::getline(file, line)) {
-		std::cout << line << std::endl;
-	}
-
 	GLFWwindow* window;
 
 	if (!glfwInit()) {
 		std::cout << "GLFW couldn't start\n";
 		return -1;
 	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 
 	window = glfwCreateWindow(1280, 720, "3D Program", NULL, NULL);
+	glfwMakeContextCurrent(window);
 
 	glfwMakeContextCurrent(window);
 
@@ -36,6 +35,12 @@ int main() {
 	}
 
 	glClearColor(0.25, 0.75, 0.5, 1.0);
+
+	int w,h;
+	glfwGetFramebufferSize(window, &w, &h);
+	glViewport(0,0,w,h);
+
+	TriangleMesh* triangle = new TriangleMesh();
 
 	unsigned int shader = make_shader(
 		"shader/vertex.txt",
@@ -47,6 +52,7 @@ int main() {
 		
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shader);
+		triangle->draw();
 
 		glfwSwapBuffers(window);
 	}
@@ -58,16 +64,25 @@ int main() {
 }
 
 unsigned int make_shader(const std::string& vertex_filepath, const std::string& fragment_filepath) {
+	
+	// Stores shader modules
 	std::vector<unsigned int> modules;
+
+	// Add a vertex shader module
 	modules.push_back(make_module(vertex_filepath, GL_VERTEX_SHADER));
+
+	// Add a fragment shader module
 	modules.push_back(make_module(fragment_filepath, GL_FRAGMENT_SHADER));
 
+
+	// Attach all modules and link program
 	unsigned int shader = glCreateProgram();
 	for (unsigned int shaderModule : modules) {
 		glAttachShader(shader, shaderModule);
 	}
 	glLinkProgram(shader);
 
+	// Test and error handle
 	int success;
 	glGetProgramiv(shader, GL_LINK_STATUS, &success);
 	if (!success) {
@@ -75,11 +90,17 @@ unsigned int make_shader(const std::string& vertex_filepath, const std::string& 
 		glGetProgramInfoLog(shader, 1024, NULL, errorLog);
 		std::cout << "Shader Linking error:\n" << errorLog << std::endl;
 	}
+	
+	// Free modules now that they are linked
+	for (unsigned int shaderModule : modules) {
+		glDeleteShader(shaderModule);
+	}
 
 	return shader;
 }
 
 unsigned int make_module(const std::string& filepath, unsigned int module_type) {
+	
 	std::ifstream file;
 	std::stringstream bufferedLines;
 	std::string line;
@@ -90,6 +111,7 @@ unsigned int make_module(const std::string& filepath, unsigned int module_type) 
 	}
 	std::string shaderSource = bufferedLines.str();
 	const char* shaderSrc = shaderSource.c_str();
+	bufferedLines.str("");
 	file.close();
 
 	unsigned int shaderModule = glCreateShader(module_type);
